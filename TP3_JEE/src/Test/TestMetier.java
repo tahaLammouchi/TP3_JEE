87c42dbf54b0be707841fe9ef354312823eab4ca
tahaLammouchi <114821317+tahaLammouchi@users.noreply.github.com>
2023-03-03 15:09:46 +0100

    Add files via upload

---
+++ TP3_JEE/src/Test/TestMetier.java
@@ -0,0 +1,17 @@
+package Test;
+
+import java.util.List;
+
+import metier.MetierImpl;
+import metier.Produit;
+
+public class TestMetier {
+	
+public static void main(String[] args) {
+MetierImpl metier= new MetierImpl();
+List<Produit> prods = metier.getProduitsParMotCle("PC");
+for (Produit p : prods)
+System.out.println(p.getNomProduit());
+}
+
+}
